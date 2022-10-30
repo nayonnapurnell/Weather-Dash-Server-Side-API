80e6bcd00ac7e83e67569c31707bb445ae37d78d
“Nayonna <Nayonna Purnell>
2022-10-30 00:28:50 -0400

    Adding the weather api and data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,49 @@
 //Get HTML Elements
-var weather_result = document.getElementsByClassName('currentCityWeather');
-var weatherSearchBtn = document.getElementById('weatherSearchResult');
 
-//Event listener for the Weather Search Button
-//weatherSearchBtn.addEventListener('click', getCurrentWeather);
+var citySearchInput = document.getElementById('citySearch');
+var stateSearchInput = document.getElementById('stateSearch');
+var weather_result = document.querySelector('.currentCityWeather');
+var countryCode = 'US';
+var tempCityDateEl = document.querySelector('#tempCityDate');
+var windCityDateEl = document.querySelector('#windCityDate');
+var humidityCityDateEl = document.querySelector('#humidityCityDate');
+
+ 
+var apiKey = '416e955c8b7d5eebd548200f74e3a752';
 
 
 
-function getApi() {
-
-var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=416e955c8b7d5eebd548200f74e3a752';
-
+function getWeather() {
+//'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=416e955c8b7d5eebd548200f74e3a752'
 
 
- fetch(requestUrl)
- //Waiting to get the response
-   .then(function (response) {
-     return response.json();
-   })
-   .then(function (data) {
-    console.log(data);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value},${stateSearchInput.value},us&appid=416e955c8b7d5eebd548200f74e3a752`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+         
     });
 
 }
-getApi();
+
+function displayWeather(data){
+  tempCityDateEl.innerText = "Temp: " + `${data}`;
+  //tempCityDateEl.innerText = `Temp: ${data}`;
+      //windCityDateEl.innerText = `Wind: ${data.wind}`;
+     // humidityCityDateEl.innerText = `Humidity: ${data.humidity}`;
+
+}
+
+
+var weatherSearchBtn = document.getElementById('weatherSearchButton');
+//citySearchInput.value = localStorage.getItem("citySearch");
+
+
+function getCityInput() {
+  //localStorage.setItem("citySearch", citySearchInput.value);
+  //humidityCityDateEl.innerText = "Loading ...";
+ getWeather();
+ displayWeather();
+}
+
+weatherSearchBtn.addEventListener("click", getCityInput);
