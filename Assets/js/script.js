e0af64241109703fb0266ac8c9f3fc480c0a4141
“Nayonna <Nayonna Purnell>
2022-10-30 21:51:48 -0400

    Added Day 1 of 5 Weather data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,6 +7,7 @@ var countryCode = 'US';
 var tempCityDateEl = document.querySelector('#tempCityDate');
 var windCityDateEl = document.querySelector('#windCityDate');
 var humidityCityDateEl = document.querySelector('#humidityCityDate');
+var cardBodyCl = document.querySelector('.card-body');
 var weatherIcon = document.querySelector('#weather-icon');
 
  
@@ -20,9 +21,9 @@ function getWeather() {
     .then(res => res.json())
     .then(data => {
       console.log(data);
-      tempCityDateEl.innerText = "Temp: " + JSON.stringify(data.main.temp) + ' \u00B0' +  "F";
+      tempCityDateEl.innerText = "Temp: " + JSON.stringify(Math.round(data.main.temp)) + ' \u00B0' +  "F";
       humidityCityDateEl.innerText = "Humidity: " + JSON.stringify(data.main.humidity) + "%";
-      windCityDateEl.innerText = "Wind: " + JSON.stringify(data.wind.speed) + " MPH";
+      windCityDateEl.innerText = "Wind: " + JSON.stringify(Math.round(data.wind.speed)) + " MPH";
       //weatherIcon.innerText = JSON.stringify(data.weather[0].icon);
       //weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
@@ -30,11 +31,33 @@ function getWeather() {
 
 }
 
-function display5DAyWeather(){
+function display5DayWeather(){
+  //5-Day Weather Forecast Information
+  var day1Date = document.querySelector('#day-1-date');
+  var day1Temp = document.querySelector('#day-1-temp');
+  var day1Wind = document.querySelector('#day-1-wind');
+  var day1Humidity = document.querySelector('#day-1-humidity');
+
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citySearchInput.value},${stateSearchInput.value},us&appid=416e955c8b7d5eebd548200f74e3a752&units=imperial`)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
+     console.log(data);
+     //Day 1 Weather
+     day1Date.innerText = "Date: " + JSON.stringify(data.list[16].dt_txt);
+     day1Temp.innerText = "Temp: " + JSON.stringify(Math.round(data.list[16].main.temp)) + ' \u00B0' +  "F";
+     day1Wind.innerText = "Wind: " + JSON.stringify(Math.round(data.list[16].wind.speed)) + " MPH";
+     day1Humidity.innerText = "Humidity: " + JSON.stringify(Math.round(data.list[16].main.humidity)) + "%";
+
+     //Day 2 Weather
+     day2Temp.innerText = "Temp: " + JSON.stringify(Math.round(data.list[16].main.temp)) + ' \u00B0' +  "F";
+
+     //Day 3 Weather
+
+     //Day 4 Weather
+
+     //Day 5 Weather
+
+   
 
     });
 }
@@ -45,7 +68,7 @@ var weatherSearchBtn = document.getElementById('weatherSearchButton');
 
 function getCityInput() {
  getWeather();
- display5DAyWeather();
+ display5DayWeather();
 }
 
 weatherSearchBtn.addEventListener("click", getCityInput);
